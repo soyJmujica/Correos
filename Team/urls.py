be9619a7dd224ebb9d4e37224c0cd85207565d33
Jose Mujica <master1495@gmail.com>
2023-12-28 11:02:02 -0400

    Team/views updated

--- Team/urls.py
+++ Team/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('addagents/', views.AddAgents, name = 'agregar'),
+path('addagents/', views.AddAgent, name = 'agregar'),
 path('agents/', views.Agents, name = 'agentes'),
 ]
