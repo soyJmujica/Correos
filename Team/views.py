11d44ff4ab6b48ad886a1f8be7a1b106e958126b
Jose Mujica <master1495@gmail.com>
2023-12-28 10:24:44 -0400

    statics and Team/views.py, Team/templates base, addagent and navbar created

--- Team/views.py
+++ Team/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import TeamMembers
+from .forms import TeamForm
 # Create your views here.
+
+def AddAgent(request):
+	if request.method == 'POST':
+		form = TeamForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('agentes')
+	else:
+		form = TeamForm()
+	return render(request, 'addagent.html', {'encabezado':'Add New Agent To The Team',
+		'form':form})
+
