faf5f3aeda99d2f5783c4770f8f77186d764776e
Jose Mujica <master1495@gmail.com>
2023-12-28 12:11:32 -0400

    all urls.py updated

--- Team/urls.py
+++ Team/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
 path('addagents/', views.AddAgent, name = 'agregar'),
 path('agents/', views.Agents, name = 'agentes'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT)
+]
