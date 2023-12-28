9a3bb4de9cbc14760ca8a4915a0639978ecc3d4b
Jose Mujica <master1495@gmail.com>
2023-12-28 12:08:48 -0400

    all urls.py updated

--- render/urls.py
+++ render/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
 path('', views.index, name = "index"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT)
+]
