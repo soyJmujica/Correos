40996259dce7ac4a317fee24f676d7f1f565436f
Jose Mujica <master1495@gmail.com>
2023-12-29 16:16:49 -0400

    models.py and forms.py updated

--- render/forms.py
+++ render/forms.py
@@ -7,4 +7,4 @@ class TeamForm(ModelForm):
 	"""docstring for TeamForm"""
 	class Meta:
 		model = TeamMembers
-		fields = ['first_name','last_name','phone','email','join']
+		fields = ['first_name','last_name','phone','email','join','picture']
--- render/models.py
+++ render/models.py
@@ -7,15 +7,13 @@ class TeamMembers(models.Model):
 	phone = models.CharField(max_length=100, verbose_name="Phone Number")
 	email = models.CharField(max_length=100, verbose_name="Email")
 	join = models.DateField(verbose_name="Joining Date")
-	#picture = models.ImageField(upload_to = 'static/img/agent_photos/',blank=True,null = True, verbose_name = "Profile Picture")
+	picture = models.ImageField(upload_to = 'static/img/agent_photos/',blank=True,null = True, verbose_name = "Profile Picture")
 	pending = models.IntegerField(default=0)
 	closed = models.IntegerField(default=0)
 
-	
+	def save(self, *args, **kwargs):
+		self.picture.name = str(self.first_name+"_"+self.last_name+".jpg")
+
 
 	def __str__(self):
 		return str(self.first_name + " " + self.last_name)
-
-
-'''def save(self, *args, **kwargs):
-			self.picture.name = str(self.first_name+"_"+self.last_name+".jpg")'''
