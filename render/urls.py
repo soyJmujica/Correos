9d26e1bc86c5882c62ad753c80ebcfa6681bbd18
Jose Mujica <master1495@gmail.com>
2023-12-29 14:02:03 -0400

    views.py and url.py updated

--- render/urls.py
+++ render/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
 path('', views.index, name = "index"),
 path('addagents/', views.AddAgent, name = 'agregar'),
 path('agents/', views.Agents, name = 'agentes'),
+path('agents/<int:agent_id>/', views.AgentInfo, name = 'detalles agente'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT)
