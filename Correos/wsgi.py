5298aa0add1b96c830fbf332bc81d2bb3d2225a4
Jose Mujica <master1495@gmail.com>
2023-12-28 21:26:16 -0400

    settings.py updated

--- Correos/wsgi.py
+++ Correos/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Correos.settings")
 
 application = get_wsgi_application()
+
+from Correos.wsgi import CorreosApplication
+
+application = CorreosApplication(application)
