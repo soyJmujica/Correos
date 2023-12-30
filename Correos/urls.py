d5fff5e0fa2a44b2d60d5e72303fc11b50b774d0
Jose Mujica <master1495@gmail.com>
2023-12-30 16:12:29 -0400

    correos/urls.py updated

--- Correos/urls.py
+++ Correos/urls.py
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('render.urls')),
+    path('', include('Buyer.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
