782e508d5b1a3f915abeffab923f5bb610b71c8c
Jose Mujica <master1495@gmail.com>
2023-12-31 17:38:00 -0400

    Buyer/template/transacciones.html & urls.py updated

--- Buyer/urls.py
+++ Buyer/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 path('undercontract/', views.undercontract, name = 'under contract'),
 path('transactions/', views.transactions, name = 'transacciones'),
-path('transactions/<int:property_id>', views.details, name = 'detalles')
+path('transactions/<int:property_id>/', views.details, name = 'detalles')
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT)
