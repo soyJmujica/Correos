df6463758ba4a169f3ed2fa2c1822675e80281e6
Jose Mujica <master1495@gmail.com>
2024-01-02 13:27:09 -0400

    template, view and urls for pending deals for each agent created

--- render/urls.py
+++ render/urls.py
@@ -9,7 +9,7 @@ path('', views.index, name = "index"),
 path('addagents/', views.AddAgent, name = 'agregar'),
 path('agents/', views.Agents, name = 'agentes'),
 path('agents/<int:agent_id>/', views.AgentInfo, name = 'detalles agente'),
-path('agents/<int:agent_id>/pending/', views.AgentsPending, name = "pendientes")
+path('agents/<int:agent_id>/pending/', views.AgentsPending, name = "pendientes"),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT)
