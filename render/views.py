b701fff1b6bd5b93c8558b0d5b8fcffc6c1a6ce2
Jose Mujica <master1495@gmail.com>
2024-01-02 12:52:39 -0400

    template, view and urls for pending deals for each agent created

--- render/views.py
+++ render/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import TeamMembers
 from .forms import TeamForm
+from Buyer.models import UnderContractBuyer
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
 
@@ -23,8 +24,47 @@ def index(request):
 	return render(request,'index.html',{})
 
 def AgentInfo(request, agent_id):
-	agente = get_object_or_404(TeamMembers, pk = agent_id)
-	return render(request, 'agentinfo.html',{'encabezado':agente.first_name, 'agent':agente})
+	agents = TeamMembers.objects.all()
+	closed = UnderContractBuyer.objects.values('agent','status').annotate(Count('agent'))
+	a = closed.values_list('agent','status','agent__count')
+	for agent in agents:
+		for count in a:
+			if agent.id == count[0]:
+				if count[1]=='Pending':
+					agent.pending = count[2]
+				elif count[1]=='Closed':
+					agent.closed = count[2]
+		agent.save()
+
+
+	return render(request, 'agents.html', {'encabezado':"Team", "agents":agents})
+
+
+
+'''
+def Agents(request):
+	agents = TeamMembers.objects.all()
+	closed = UnderContractBuyer.objects.values('agent','status').annotate(Count('agent'))
+	a = closed.values_list('agent','status','agent__count')
+	for agent in agents:
+		for count in a:
+			if agent.id == count[0]:
+				if count[1]=='Pending':
+					agent.pending = count[2]
+				elif count[1]=='Closed':
+					agent.closed = count[2]
+		agent.save()
+
+
+	return render(request, 'agents.html', {'encabezado':"Team", "agents":agents})'''
+
+
+
+def AgentsPending(request, agent_id):
+	properties = UnderContractBuyer.objects.filter(agent = agent_id, status = "Pending")
+
+	return render(request, 'pendientes.html',{'encabezado':f"Pending Deals Of {properties.agent_id}",
+		"properties":properties})
 
 
 
