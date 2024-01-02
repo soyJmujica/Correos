e6d8df04b153d765426e96c888cf93d8de39782e
Jose Mujica <master1495@gmail.com>
2024-01-02 13:16:21 -0400

    template, view and urls for pending deals for each agent created

--- render/models.py
+++ render/models.py
@@ -7,7 +7,6 @@ class TeamMembers(models.Model):
 	phone = models.CharField(max_length=100, verbose_name="Phone Number")
 	email = models.CharField(max_length=100, verbose_name="Email")
 	join = models.DateField(verbose_name="Joining Date")
-	#picture = models.ImageField(upload_to = 'agent_photos/',blank=True,null = True, verbose_name = "Profile Picture")
 	pending = models.IntegerField(default=0)
 	closed = models.IntegerField(default=0)
 
