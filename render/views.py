b053117b620dc1159982673293b08866f8ec111c
Jose Mujica <master1495@gmail.com>
2024-01-02 21:16:37 -0400

    template, view and urls for pending deals for each agent created

--- render/views.py
+++ render/views.py
@@ -17,31 +17,11 @@ def AddAgent(request):
 	return render(request, 'addagent.html', {'encabezado':'Add New Agent To The Team',
 		'form':form})
 
-def Agents(request):
-	agentes=TeamMembers.objects.all()
-	return render(request, 'agents.html', {'encabezado':"Team", 'agents':agentes})
-
 def index(request):
 	return render(request,'index.html',{})
 
-def AgentInfo(request, agent_id):
-	agents = TeamMembers.objects.all()
-	closed = UnderContractBuyer.objects.values('agent','status').annotate(Count('agent'))
-	a = closed.values_list('agent','status','agent__count')
-	for agent in agents:
-		for count in a:
-			if agent.id == count[0]:
-				if count[1]=='Pending':
-					agent.pending = count[2]
-				elif count[1]=='Closed':
-					agent.closed = count[2]
-		agent.save()
-
-	return render(request, 'agentinfo.html', {'encabezado':"Team", "agents":agents})
 
 
-
-'''
 def Agents(request):
 	agents = TeamMembers.objects.all()
 	closed = UnderContractBuyer.objects.values('agent','status').annotate(Count('agent'))
@@ -56,7 +36,12 @@ def Agents(request):
 		agent.save()
 
 
-	return render(request, 'agents.html', {'encabezado':"Team", "agents":agents})'''
+	return render(request, 'agents.html', {'encabezado':"Team", "agents":agents})
+
+
+def AgentInfo(request, agent_id):
+	agente = get_object_or_404(TeamMembers, pk = agent_id)
+	return render(request, 'agentinfo.html',{'encabezado':agente.first_name, 'agent':agente})
 
 
 
