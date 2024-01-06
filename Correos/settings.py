8c287e4d71a77c47db2310dc7e465914ed233cf1
Jose Mujica <master1495@gmail.com>
2024-01-05 21:18:10 -0400

    adding to the navbar the email sending function

--- Correos/settings.py
+++ Correos/settings.py
@@ -154,9 +154,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # settings.py
 DATE_INPUT_FORMATS = ['%d-%m-%Y', '%Y-%m-%d', '%m/%d/%Y','%d/%m/%Y']
 
-#Email info
+#Email
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'EMAIL_ACCOUNT'
+EMAIL_HOST_USER = 'master1495@gmail.com'
 EMAIL_HOST_PASSWORD = 'EMAIL_PASSWORD'
 EMAIL_USE_TLS = True
