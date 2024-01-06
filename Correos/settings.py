b979824fb9edc914a676b198986bbd675b28afd2
Jose Mujica <master1495@gmail.com>
2024-01-06 10:44:55 -0400

    settings.py updated

--- Correos/settings.py
+++ Correos/settings.py
@@ -157,9 +157,9 @@ DATE_INPUT_FORMATS = ['%d-%m-%Y', '%Y-%m-%d', '%m/%d/%Y','%d/%m/%Y']
 #Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'master1495@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'EMAIL_HOST_USER'
 EMAIL_HOST_PASSWORD = 'EMAIL_PASSWORD'
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 EMAIL_USE_SSL = True
 
